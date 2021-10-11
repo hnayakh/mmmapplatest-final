1db08b40e2e86acdf30f2cf7fe306aef9b07e87f
Amrit Rath <amritrath327@gmail.com>
2021-10-11 15:42:08 +0530

    Amrit commit on profile details

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -15,7 +15,7 @@ class AppConstants {
   static final EMAILREGEXP =
       r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
   static final PASSWORDREGEXP =
-      r"^[a-zA-Z0-9]+$";
+      r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&#]{8,16}$";
 
   static final SUCCESS = "SUCCESS";
   static final FAILURE = "FAILURE";
